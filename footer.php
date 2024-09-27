3e9bd1f8684a2667bd48c08a3e275089c2114c50
anatomyos <147455292+anatomyos@users.noreply.github.com>
2024-09-27 19:45:13 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+        <footer>
+            <hr>
+            <p>&copy; <?php echo date('Y'); ?> Anesthesia Department</p>
+        </footer>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+</html>
